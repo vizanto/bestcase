369793b6170ae76e8d3ed8922c3b830f2d7df1b5
Danny Wilson <danny@prime.vc>
2016-05-23 21:31:51 +0200

    Release fork

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.cpclermont/bestcase "0.2.1"
+(defproject vizanto/bestcase "0.2.2.3"
   :description "An A/B testing library for clojure"
   :url "https://github.com/charlespwd/bestcase"
   :license {:name "Eclipse Public License"
